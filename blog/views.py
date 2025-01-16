924a3e80db49a4ba53b9a7e0125d28d4e4317468
offcwho <nhnhnhbgbg@gmail.com>
2025-01-16 19:03:34 +0300

    save

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Category, Post
 # Create your views here.
+def index(request):
+    posts = Post.objects.filter(is_active=True).order_by('-id')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'index.html', context)
